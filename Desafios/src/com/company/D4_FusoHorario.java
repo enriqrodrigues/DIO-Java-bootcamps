68031d93d8a9865f09bb049c17538749954e045d
Henrique Rodrigues <mandemsgs@hotmail.com>
2021-11-27 14:42:09 -0300

    Desafios da primeira parte

---
+++ Desafios/src/com/company/D4_FusoHorario.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class D4_FusoHorario {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int horaSaida = sc.nextInt();
+        int tempoViagem = sc.nextInt();
+        int fuso = sc.nextInt();
+
+        int horaChegada = horaSaida + tempoViagem + fuso;
+        if(horaChegada>24){
+            horaChegada -= 24;
+        }
+
+        System.out.println(horaChegada);
+
+        sc.close();
+    }
+}
---
+++ Desafios/src/com/company/D5_ParImpar.java
@@ -0,0 +1,33 @@
+package com.company;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class D5_ParImpar {
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        for (int i = 0; i < N; i++) {
+            int numero = leitor.nextInt();
+                                //Testa se o número é par ou ímpar.
+            String paridade;
+            if((numero%2)==0){
+                paridade = "EVEN";
+            } else {
+                paridade = "ODD";
+            }
+                                //Testa se o número é positivo ou negativo.
+            String sinal;
+            if(numero<0){
+                sinal = "NEGATIVE";
+            }else{
+                sinal = "POSITIVE";
+            }
+                                //Se o número for 0 ele escreve o texto padrão "NULL".
+            if(numero == 0){
+                paridade = "NULL";
+                sinal = "";
+            }
+            System.out.println(paridade + " " + sinal);
+        }
+    }
+}
